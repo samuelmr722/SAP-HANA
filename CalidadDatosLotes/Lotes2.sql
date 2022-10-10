5ca0d245a845d131bfb6f454ddf9fb03d09afcaf
samuelmr722 <34388939+samuelmr722@users.noreply.github.com>
2022-10-10 17:43:13 -0500

    Update Lotes2.sql

--- CalidadDatosLotes/Lotes2.sql
+++ CalidadDatosLotes/Lotes2.sql
@@ -5,7 +5,7 @@ create procedure "_SYS_BIC"."CD.LOTES/ZCV_LOTES_CALIDAD/proc" ( IN I_MTART NVARC
 --  PAQUETE          : CD
 --  FECHA DE CREACION: 15/05/2018
 --  MODULO SAP       : WM
---  AUTOR (ID/NOMBRE): MUNOZSAM/Samuel Muñoz
+--  AUTOR (ID/NOMBRE): Samuel
 --  TITULO           : Calidad de datos para lotes
 --  TIPO             : Reporte
 --  USO              : Composite en BW
